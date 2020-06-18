e4da394513e13307589131c5487f982af9cb8e46
unknown <xkevin24x@gmail.com>
2020-06-17 19:53:01 -0500

    erro bd

--- src/keys.js
+++ src/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
 
 database: {
-URI:'mongodb+srv://root:<root>@cluster0-suo1l.mongodb.net/Red-Social?retryWrites=true&w=majority'
+URI:'mongodb+srv://root:root@cluster0-suo1l.mongodb.net/Red-Social?retryWrites=true&w=majority'
 
 }
 
